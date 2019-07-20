5ff93b6bd09888bcf8b39a14d98310f83a0dfcb1
github username <github email address>
2019-07-20 16:21:01 -0400

    seeding the data from openBreweryDB

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,35 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
+require 'rest-client'
+
+page = 1
+
+# url = "https://api.openbrewerydb.org/breweries?by_state=pennsylvania&page=#{page}&per_page=50"
+
+while page < 9 do
+
+    url = "https://api.openbrewerydb.org/breweries?by_state=pennsylvania&page=#{page}&per_page=50"
+
+    data = JSON.parse(RestClient.get(url).body)
+    data.each do |brewery|
+        Brewery.create(
+            name: brewery['name'],
+            brewery_type: brewery['brewery_type'],
+            street: brewery['street'],
+            city: brewery['city'],
+            state: brewery['state'],
+            postal_code: brewery['postal_code'],
+            country: brewery['country'],
+            longitude: brewery['longitude'],
+            latitude: brewery['latitude'],
+            phone: brewery['phone'],
+            website_url: brewery['website_url'],
+            rating: 5,
+            
+        )
+    end
+
+    page += 1
+end
 
